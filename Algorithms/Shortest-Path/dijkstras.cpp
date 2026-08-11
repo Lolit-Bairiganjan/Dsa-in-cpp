430ed026f7f180f681758f18296425fee9c430f6
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2026-08-11 22:41:42 +0530

    Implemented dijkstras algorithm

--- Algorithms/Shortest-Path/dijkstras.cpp
+++ Algorithms/Shortest-Path/dijkstras.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<queue>
 
 using namespace std;
+
+const int INF = 1e9;
 
 template <typename T>
 class Graph{
@@ -13,17 +16,56 @@ class Graph{
             adjList[u].push_back({v, weight});
             if(!isDirected) adjList[v].push_back({u, weight});
         }
+
         void printGraph(){
             for(const auto &[node, neighbours] : adjList){
                 cout << node << " -> ";
 
                 for(const auto &neighbour : neighbours){
                     cout << "(" << neighbour.first << ", " << neighbour.second << ") ";
+                }
+
+            cout << endl;
+            }
         }
 
-        cout << endl;
-    }
-}
+        void dijkstra(const T &source){
+            unordered_map<T, int>dist;
+
+            for(const auto &pair : adjList){
+                dist[pair.first] = INF;
+            }
+            using pii = pair<int, T>;
+            priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+            pq.push({0, source});
+            dist[source] = 0;
+
+            while(!pq.empty()){
+                const auto [curr_dist, u] = pq.top();
+                pq.pop();
+                // Not using visited hash_map to check if a node was visited. Using this instead
+                if(curr_dist > dist[u]) continue;
+
+                for(const auto &edge : adjList[u]){
+                    T v = edge.first;
+                    int weight = edge.second;
+
+                    if(dist[u] + weight < dist[v]){
+                        dist[v] = dist[u] + weight;
+                        pq.push({dist[v], v});
+                    } 
+                }
+            }
+            std::cout << "--- Shortest Distances from " << source << " ---\n";
+            for (const auto& [node, d] : dist) {
+                if (d == INF) {
+                    std::cout << source << " to " << node << " : Unreachable\n";
+                } else {
+                    std::cout << source << " to " << node << " : " << d << "\n";
+                }
+            }
+        }
 };
 
 int main()
@@ -37,5 +79,7 @@ int main()
     g.addEdge(2, 3, 5);
 
     g.printGraph();
+
+    g.dijkstra(0);
     return 0;
 }
